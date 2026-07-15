3cfbdce4c2ebb51887f28aedc9625ef43b421fa0
Janhvi Gupta <janhvigupta2402@gmail.com>
2026-07-15 14:48:30 +0530

    Correct the route of model

--- PPO_SUMO/test.py
+++ PPO_SUMO/test.py
@@ -11,8 +11,8 @@ from baseline import run_offset_fixed_time
 from env import SumoTrafficEnv2J
 
 SCRIPT_DIR      = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH      = os.path.join(SCRIPT_DIR, "models", "sweep_sp03" ,"ppo_sumo_2junction")
-NORMALIZER_PATH = os.path.join(SCRIPT_DIR, "models","sweep_sp03" , "vec_normalize_sumo.pkl")
+MODEL_PATH      = os.path.join(SCRIPT_DIR, "models", "ppo_sumo_2junction")
+NORMALIZER_PATH = os.path.join(SCRIPT_DIR, "models", "vec_normalize_sumo.pkl")
 
 # --- set True when you want to record a video, False for fast headless eval ---
 RECORD = False
